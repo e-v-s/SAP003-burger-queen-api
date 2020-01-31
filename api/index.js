d14cac755fb6a36d0ff17eb5956ab5ebe7b8816f
evs <evoraleite@gmail.com>
2020-01-31 13:29:22 -0300

    heroku

--- api/index.js
+++ api/index.js
@@ -1,4 +1,4 @@
-import express from 'express';
+import express from '../express';
 import bodyParser from 'body-parser';
 import ProductsRoutes from './server/routes/ProductsRoutes';
 import TablesRoutes from './server/routes/TablesRoutes';
@@ -11,9 +11,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 const port = process.env.PORT || 3000;
 
-app.use('/api/Products', ProductsRoutes);
-app.use('/api/Tables', TablesRoutes)
-app.use('/api/Orders', OrdersRoutes)
+app.use('./api/Products', ProductsRoutes);
+app.use('./api/Tables', TablesRoutes)
+app.use('./api/Orders', OrdersRoutes)
 
 // quando recebe uma rota não listada
 app.get('*', (req, res) => res.status(200).send({
